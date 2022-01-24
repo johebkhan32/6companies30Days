70c4e29108df8729bd500a420d329d302820f536
johebkhan32 <johebkhan32@gmail.com>
2022-01-24 18:08:13 +0530

    added Adobe and Intuit folder

---
+++ Adobe/Amend_The_Sentence11.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    string amendSentence(string s)
+    {
+        // your code here
+        if (isupper(s[0]))
+            s[0] = tolower(s[0]);
+        string ans = "";
+        int i = 0, j = 0;
+        while (j < s.length())
+        {
+            if (isupper(s[j]))
+            {
+                s[j] = tolower(s[j]);
+                ans += s.substr(i, j - i);
+                ans += " ";
+                i = j;
+            }
+            j++;
+        }
+        ans += s.substr(i, j - i);
+        return ans;
+    }
+};
